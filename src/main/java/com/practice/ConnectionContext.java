2ff06563a79740dc88da17a320039f8c75ee1219
Karthikeyan <keyan9329@gmail.com>
2026-05-20 12:09:39 +0530

    Just sending the incoming data

--- src/main/java/com/practice/ConnectionContext.java
+++ src/main/java/com/practice/ConnectionContext.java
@@ -1,9 +1,15 @@
 package com.practice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
 
 public class ConnectionContext {
+    private static final Logger logger = LoggerFactory.getLogger(ConnectionContext.class);
     private static final int KB = 1024;
     private static final int BUFFER_SIZE = 16 * KB;
     private static int connectionIdCounter = 0;
@@ -17,12 +23,26 @@ public class ConnectionContext {
         this.writeBuffer  = ByteBuffer.allocate(BUFFER_SIZE);
     }
     
-    public void read(SelectionKey selectionKey) {
-        
+    public void read(SelectionKey selectionKey) throws IOException {
+        var channel = (SocketChannel) selectionKey.channel();
+        int r = channel.read(readBuffer);
+        if (r == -1) {
+            channel.close();
+            selectionKey.cancel();
+            logger.info("Closed connection {}", this.connectionId);
+        } else if (r > 0) {
+            readBuffer.flip();
+            selectionKey.interestOps(SelectionKey.OP_WRITE);
+        }
     }
     
-    public void write(SelectionKey selectionKey) {
-        
+    public void write(SelectionKey selectionKey) throws IOException {
+        var channel = (SocketChannel) selectionKey.channel();
+        channel.write(readBuffer);
+        if (!readBuffer.hasRemaining()) {
+            readBuffer.clear();
+            selectionKey.interestOps(SelectionKey.OP_READ);
+        }
     }
     
     public static ConnectionContext getInstance() {
